3c202425bebe1e143ebeb38d32a8593bdfae4439
Siddharth Sonti <83441624+Sonti974948@users.noreply.github.com>
2025-12-05 17:02:18 -0800

    Change SLURM account and modify PLUMED settings
    Updated SLURM account and adjusted PLUMED parameters.

--- Metadynamics/run.sh
+++ Metadynamics/run.sh
@@ -6,7 +6,7 @@
 #SBATCH --gpus=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=50G
-#SBATCH --account=ucd187
+#SBATCH --account=ucd239
 #SBATCH --no-requeue
 #SBATCH --mail-user=youremail@ucdavis.edu
 #SBATCH --mail-type=ALL
@@ -29,5 +29,5 @@ echo $AMBERHOME
 #export PLUMED_KERNEL=/home/ssonti/miniconda3/lib/libplumedKernel.so
 export PLUMED_KERNEL=/cm/shared/apps/spack/gpu/opt/spack/linux-centos8-skylake_avx512/gcc-8.3.1/plumed-2.6.1-63lfaa2clqpjeif3aa3kdk44ozvlzkac/lib/libplumedKernel.so
 pmemd.cuda -O -i md.in -o md.out -p chignolin.prmtop -c chignolin.rst -r md.rst -x md.nc
-plumed sum_hills --hills HILLS --mintozero --min 0  --max 10 --bin 100
+plumed sum_hills --hills HILLS --min 0  --max 1 --bin 100
 
